96a3202511ca83112c5038eedd2b402feeec0cf2
Eugene <ekarachkovsky@gmail.com>
2018-05-12 12:02:28 +0600

    Init

---
+++ Sotomarket/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sotomarket.Startup))]
+namespace Sotomarket
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
